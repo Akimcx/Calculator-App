4fc131b2014597f667e2e53cc825829f1468bbed
ACACIA Kassim A <kacacia24@gmail.com>
2020-04-13 23:28:38 -0400

    setting up the repo

--- js/calc2.js
+++ js/calc2.js
@@ -1,346 +1,177 @@
-let calc = function() {
-   createAndAddNumbers();
-   const buttons = document.querySelectorAll(".calc-btn");
-   let label = document.querySelector(".label");
-   let queue = document.querySelector(".queue");
-   const operators = /\+|\/|\*|-/;
-   const operatorBtns = document.querySelectorAll(".calc-btn-operator");
-   let equalBtnPressed = false;
-   let isFloat = false;
-   label.innerHTML = "";
+createButton();
+const buttons = document.querySelectorAll(".calc-btn");
+const calculator = document.getElementById("calc")
+let result = document.querySelector(".result");
+let operation = document.querySelector(".operation");
+const operators = /\+|\/|\*|-/;
+const operatorBtns = document.querySelectorAll(".calc-btn-operator");
+let equalBtnPressed = false;
+let isFloat = false;
+result.innerHTML = "";
 
-   document.addEventListener("keydown", e => {
-      if (/\d|Backspace|Enter/.test(e.key)) {
-         afficheNombre(e.key);
-      }
-   });
+document.addEventListener("keydown", e => {
+  if (/\d|Backspace|Enter/.test(e.key)) {
+    afficheNombre(e.key);
+  }
+});
 
-   buttons.forEach(button => {
-      button.addEventListener("click", e => {
-         afficheNombre(button.innerHTML);
-      });
-   });
+buttons.forEach((button) => {
+  button.addEventListener("click", e => {
+    afficheNombre(button.innerHTML);
+  });
+});
 
-   function afficheNombre(nombre) {
-      if (nombre === "=" || nombre === "Enter") {
-         if (queue.innerHTML === "" || label.innerHTML === "") {
-            return;
-         }
-         equalBtnPressed = true;
-         performOperation();
-         label.innerHTML = queue.innerHTML;
-         queue.innerHTML = "";
-         return;
-      }
+function afficheNombre(nombre) {
+  if (nombre === "=" || nombre === "Enter") {
+    if (operation.innerHTML === "" || result.innerHTML === "") {
+      return;
+    }
+    equalBtnPressed = true;
+    performOperation();
+    result.innerHTML = operation.innerHTML;
+    operation.innerHTML = "";
+    return;
+  }
 
-      if (nombre === "&lt;" || nombre === "Backspace") {
-         let char = label.innerHTML.charAt(label.innerHTML.length - 1);
-         let index = label.innerHTML.lastIndexOf(char);
-         let tab = Array.from(label.innerHTML);
-         tab[index] = "";
-         label.innerHTML = tab.join("");
-         if (label.innerHTML.length === 0) {
-            label.innerHTML = "";
-         }
-         preventDivisionByZero();
-         return;
-      }
+  if (nombre === "&lt;" || nombre === "Backspace") {
+    let char = result.innerHTML.charAt(result.innerHTML.length - 1);
+    let index = result.innerHTML.lastIndexOf(char);
+    let tab = Array.from(result.innerHTML);
+    tab[index] = "";
+    result.innerHTML = tab.join("");
+    if (result.innerHTML.length === 0) {
+      result.innerHTML = "";
+    }
+    preventDivisionByZero();
+    return;
+  }
 
-      if (nombre === "C") {
-         label.innerHTML = "";
-         queue.innerHTML = "";
-         preventDivisionByZero();
-         return;
-      }
+  if (nombre === "C") {
+    result.innerHTML = "";
+    operation.innerHTML = "";
+    preventDivisionByZero();
+    return;
+  }
 
-      if (equalBtnPressed) {
-         label.innerHTML = "";
-         equalBtnPressed = false;
-      }
+  if (equalBtnPressed) {
+    result.innerHTML = "";
+    equalBtnPressed = false;
+  }
 
-      if (nombre === "-") {
-         managingNegativeNumber();
-         return;
-      }
+  if (nombre === "-") {
+    managingNegativeNumber();
+    return;
+  }
 
-      if (nombre === "+" || nombre === "/" || nombre === "*") {
-         if (label.innerHTML === "") {
-            changeOperator(nombre);
-            return;
-         }
-         if (mustPerformOperation()) {
-            performOperation();
-            label.innerHTML = "";
-            return;
-         }
+  if (nombre === "+" || nombre === "/" || nombre === "*") {
+    if (result.innerHTML === "") {
+      changeOperator(nombre);
+      return;
+    }
+    if (mustPerformOperation()) {
+      performOperation();
+      result.innerHTML = "";
+      return;
+    }
 
-         queue.innerHTML = label.innerHTML + nombre;
-         label.innerHTML = "";
-      } else {
-         label.innerHTML += nombre;
-      }
-      preventDivisionByZero();
+    operation.innerHTML = result.innerHTML + nombre;
+    result.innerHTML = "";
+  } else {
+    result.innerHTML += nombre;
+  }
+  preventDivisionByZero();
+}
+
+function preventDivisionByZero() {
+  if (operation.innerHTML === "") {
+    return;
+  }
+  if (
+    (/\//.test(operation.innerHTML) && result.innerHTML === "0") ||
+    (/\//.test(operation.innerHTML) && result.innerHTML === "0.") ||
+    (/\//.test(operation.innerHTML) && /^\..*/.test(result.innerHTML))
+  ) {
+    operatorBtns.forEach((operator) => {
+      operator.classList.add("disable-btn");
+    });
+  } else {
+    operatorBtns.forEach((operator) => {
+      operator.classList.remove("disable-btn");
+    });
+  }
+}
+
+function mustPerformOperation() {
+  return operators.test(operation.innerHTML);
+}
+
+function performOperation() {
+  isFloat = false;
+  const operator = operation.innerHTML.slice(operation.innerHTML.length - 1);
+  const leftSide = floatOrInt(operation.innerHTML);
+  const rightSide = floatOrInt(result.innerHTML);
+
+  switch (operator) {
+    case "+":
+      operation.innerHTML = leftSide + rightSide;
+      break;
+    case "-":
+      operation.innerHTML = leftSide - rightSide;
+      break;
+    case "*":
+      operation.innerHTML = leftSide * rightSide;
+      break;
+    case "/":
+      operation.innerHTML = leftSide / rightSide;
+      break;
+  }
+  if (isFloat || /\./.test(operation.innerHTML)) {
+    operation.innerHTML = parseFloat(operation.innerHTML).toFixed(4);
+  }
+  if (!equalBtnPressed) {
+    operation.innerHTML += operator;
+  }
+}
+
+function floatOrInt(params) {
+  if (/\./.test(operation.innerHTML) || /\./.test(result.innerHTML)) {
+    isFloat = true;
+    return parseFloat(params);
+  } else {
+    return parseInt(params);
+  }
+}
+
+function managingNegativeNumber() {
+  if (result.innerHTML === "") {
+    if (operation.innerHTML === "") {
+      result.innerHTML += "-";
+    } else {
+      changeOperator("-");
+    }
+  } else {
+    if (operation.innerHTML != "") {
+      performOperation();
+      result.innerHTML = "";
+      return;
+    }
+    operation.innerHTML = result.innerHTML + "-";
+    result.innerHTML = "";
+  }
+}
+
+function changeOperator(newOperator) {
+  let str = Array.from(operation.innerHTML);
+  str[operation.innerHTML.length - 1] = newOperator;
+  operation.innerHTML = str.join("");
+}
+
+function createButton() {
+
+   for (let i = 0; i < 10; i++) {
+      const button = document.createElement("button");
+      button.classList.add("calc-btn", `calc-btn-${i}`);
+      button.innerText = i
+      calc.appendChild(button)
    }
-
-   function preventDivisionByZero() {
-      if (queue.innerHTML === "") {
-         return;
-      }
-      if (
-         (/\//.test(queue.innerHTML) && label.innerHTML === "0") ||
-         (/\//.test(queue.innerHTML) && label.innerHTML === "0.") ||
-         (/\//.test(queue.innerHTML) && /^\..*/.test(label.innerHTML))
-      ) {
-         operatorBtns.forEach(operator => {
-            operator.classList.add("disable-btn");
-         });
-      } else {
-         operatorBtns.forEach(operator => {
-            operator.classList.remove("disable-btn");
-         });
-      }
-   }
-
-   function mustPerformOperation() {
-      return operators.test(queue.innerHTML);
-   }
-
-   function performOperation() {
-      isFloat = false;
-      const operator = queue.innerHTML.slice(queue.innerHTML.length - 1);
-      const leftSide = floatOrInt(queue.innerHTML);
-      const rightSide = floatOrInt(label.innerHTML);
-
-      switch (operator) {
-         case "+":
-            queue.innerHTML = leftSide + rightSide;
-            break;
-         case "-":
-            queue.innerHTML = leftSide - rightSide;
-            break;
-         case "*":
-            queue.innerHTML = leftSide * rightSide;
-            break;
-         case "/":
-            queue.innerHTML = leftSide / rightSide;
-            break;
-      }
-      if (isFloat || /\./.test(queue.innerHTML)) {
-         queue.innerHTML = parseFloat(queue.innerHTML).toFixed(4);
-      }
-      if (!equalBtnPressed) {
-         queue.innerHTML += operator;
-      }
-   }
-
-   function floatOrInt(params) {
-      if (/\./.test(queue.innerHTML) || /\./.test(label.innerHTML)) {
-         isFloat = true;
-         return parseFloat(params);
-      } else {
-         return parseInt(params);
-      }
-   }
-
-   function managingNegativeNumber() {
-      if (label.innerHTML === "") {
-         if (queue.innerHTML === "") {
-            label.innerHTML += "-";
-         } else {
-            changeOperator("-");
-         }
-      } else {
-         if (queue.innerHTML != "") {
-            performOperation();
-            label.innerHTML = "";
-            return;
-         }
-         queue.innerHTML = label.innerHTML + "-";
-         label.innerHTML = "";
-      }
-   }
-
-   function changeOperator(newOperator) {
-      let str = Array.from(queue.innerHTML);
-      str[queue.innerHTML.length - 1] = newOperator;
-      queue.innerHTML = str.join("");
-   }
-
-   function createAndAddNumbers() {
-      for (let i = 0; i < 10; i++) {
-         let button = createButton(i, `calc-btn-${i}`);
-         let calc = document.querySelector(".calc");
-         calc.appendChild(button);
-      }
-   }
-
-   function createButton(text, className) {
-      let button = document.createElement("button");
-      button.innerText = text;
-      button.classList.add("calc-btn");
-      button.classList.add(className);
-      return button;
-   }
-};
-
-calc();
-// let calc = function() {
-//    const buttons = document.querySelectorAll(".calc-btn");
-//    let label = document.querySelector(".label");
-//    let queue = document.querySelector(".queue");
-//    const operators = /\+|\/|\*|-/;
-//    const operatorBtns = document.querySelectorAll(".calc-btn-operator");
-//    let equalBtnPressed = false;
-//    let isFloat = false;
-//    label.innerHTML = "";
-
-//    buttons.forEach(button => {
-//       button.addEventListener("click", () => {
-//          if (button.innerHTML === "=") {
-//             if (queue.innerHTML === "" || label.innerHTML === "") {
-//                return;
-//             }
-//             equalBtnPressed = true;
-//             performOperation();
-//             label.innerHTML = queue.innerHTML;
-//             queue.innerHTML = "";
-//             return;
-//          }
-
-//          if (button.innerHTML === "&lt;") {
-//             let char = label.innerHTML.charAt(label.innerHTML.length - 1);
-//             label.innerHTML = label.innerHTML.replace(char, "");
-//             if (label.innerHTML.length === 0) {
-//                label.innerHTML = "";
-//             }
-//             preventDivisionByZero();
-//             return;
-//          }
-
-//          if (button.innerHTML === "C") {
-//             label.innerHTML = "";
-//             queue.innerHTML = "";
-//             preventDivisionByZero();
-//             return;
-//          }
-
-//          if (equalBtnPressed) {
-//             label.innerHTML = "";
-//             equalBtnPressed = false;
-//          }
-
-//          if (button.innerHTML === "-") {
-//             managingNegativeNumber();
-//             return;
-//          }
-
-//          if (
-//             button.innerHTML === "+" ||
-//             button.innerHTML === "/" ||
-//             button.innerHTML === "*"
-//          ) {
-//             if (label.innerHTML === "") {
-//                changeOperator(button.innerHTML);
-//                return;
-//             }
-//             if (mustPerformOperation()) {
-//                performOperation();
-//                label.innerHTML = "";
-//                return;
-//             }
-
-//             queue.innerHTML = label.innerHTML + button.innerHTML;
-//             label.innerHTML = "";
-//          } else {
-//             label.innerHTML += button.innerHTML;
-//          }
-//          preventDivisionByZero();
-//       });
-//    });
-
-//    function preventDivisionByZero() {
-//       if (queue.innerHTML === "") {
-//          return;
-//       }
-//       if (
-//          (/\//.test(queue.innerHTML) && label.innerHTML === "0") ||
-//          (/\//.test(queue.innerHTML) && label.innerHTML === "0.") ||
-//          (/\//.test(queue.innerHTML) && /^\..*/.test(label.innerHTML))
-//       ) {
-//          operatorBtns.forEach(operator => {
-//             operator.classList.add("disable-btn");
-//          });
-//       } else {
-//          operatorBtns.forEach(operator => {
-//             operator.classList.remove("disable-btn");
-//          });
-//       }
-//    }
-
-//    function mustPerformOperation() {
-//       return operators.test(queue.innerHTML);
-//    }
-
-//    function performOperation() {
-//       isFloat = false;
-//       const operator = queue.innerHTML.slice(queue.innerHTML.length - 1);
-//       const leftSide = floatOrInt(queue.innerHTML);
-//       const rightSide = floatOrInt(label.innerHTML);
-
-//       switch (operator) {
-//          case "+":
-//             queue.innerHTML = leftSide + rightSide;
-//             break;
-//          case "-":
-//             queue.innerHTML = leftSide - rightSide;
-//             break;
-//          case "*":
-//             queue.innerHTML = leftSide * rightSide;
-//             break;
-//          case "/":
-//             queue.innerHTML = leftSide / rightSide;
-//             break;
-//       }
-//       if (isFloat || /\./.test(queue.innerHTML)) {
-//          queue.innerHTML = parseFloat(queue.innerHTML).toFixed(4);
-//       }
-//       if (!equalBtnPressed) {
-//          queue.innerHTML += operator;
-//       }
-//    }
-
-//    function floatOrInt(params) {
-//       if (/\./.test(queue.innerHTML) || /\./.test(label.innerHTML)) {
-//          isFloat = true;
-//          return parseFloat(params);
-//       } else {
-//          return parseInt(params);
-//       }
-//    }
-
-//    function managingNegativeNumber() {
-//       if (label.innerHTML === "") {
-//          if (queue.innerHTML === "") {
-//             label.innerHTML += "-";
-//          } else {
-//             changeOperator("-");
-//          }
-//       } else {
-//          if (queue.innerHTML != "") {
-//             performOperation();
-//             label.innerHTML = "";
-//             return;
-//          }
-//          queue.innerHTML = label.innerHTML + "-";
-//          label.innerHTML = "";
-//       }
-//    }
-
-//    function changeOperator(newOperator) {
-//       let str = Array.from(queue.innerHTML);
-//       str[queue.innerHTML.length - 1] = newOperator;
-//       queue.innerHTML = str.join("");
-//    }
-// };
-
-// calc();
+}
