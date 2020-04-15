e0475822e9e4b43a654e78ded737d34563da7f5a
ACACIA Kassim A <kacacia24@gmail.com>
2020-04-14 23:31:47 -0400

    Design revisted, code much readable

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Calculator App
+I revisited the design, and refactor the js code
+Now it look good to me
+Maybe need some comment
--- js/calc2.js
+++ js/calc2.js
@@ -1,177 +1,108 @@
 createButton();
 const buttons = document.querySelectorAll(".calc-btn");
 const calculator = document.getElementById("calc")
-let result = document.querySelector(".result");
-let operation = document.querySelector(".operation");
-const operators = /\+|\/|\*|-/;
-const operatorBtns = document.querySelectorAll(".calc-btn-operator");
-let equalBtnPressed = false;
-let isFloat = false;
-result.innerHTML = "";
+const operators = document.querySelectorAll(".calc-btn-operator")
+const resultCss = document.querySelector(".result");
+const operationCss = document.querySelector(".operation");
+let result = resultCss.innerText
+let operation = operationCss.innerText
 
-document.addEventListener("keydown", e => {
-  if (/\d|Backspace|Enter/.test(e.key)) {
-    afficheNombre(e.key);
-  }
-});
+document.addEventListener("keypress", e => {
+  handleEvent(e.key)
+})
 
-buttons.forEach((button) => {
+buttons.forEach(button => {
   button.addEventListener("click", e => {
-    afficheNombre(button.innerHTML);
-  });
-});
+    handleEvent(button.innerText)
+  })
+})
 
-function afficheNombre(nombre) {
-  if (nombre === "=" || nombre === "Enter") {
-    if (operation.innerHTML === "" || result.innerHTML === "") {
-      return;
+function handleEvent(text) {
+  const operator = operation.charAt(operation.length - 1)
+  if(text === "Enter") {
+    if(operation.length == 0 || result.length == 0) return
+    doOperation()
+    return
+  }
+
+  if(text === "C"){
+    result = ""
+    operation = ""
+    return
+  }
+
+  if(text === "<"){
+    result = result.replace(result.charAt(result.length - 1), "")
+    return
+  }
+
+  if("/*-+".includes(text)){
+    if(result.length == 0 && operation.length == 0) return
+    if(result.length == 0 && operation.length != 0){
+      operation = operation.replace(operator,text)
+      show()
+      return
     }
-    equalBtnPressed = true;
-    performOperation();
-    result.innerHTML = operation.innerHTML;
-    operation.innerHTML = "";
-    return;
-  }
-
-  if (nombre === "&lt;" || nombre === "Backspace") {
-    let char = result.innerHTML.charAt(result.innerHTML.length - 1);
-    let index = result.innerHTML.lastIndexOf(char);
-    let tab = Array.from(result.innerHTML);
-    tab[index] = "";
-    result.innerHTML = tab.join("");
-    if (result.innerHTML.length === 0) {
-      result.innerHTML = "";
-    }
-    preventDivisionByZero();
-    return;
-  }
-
-  if (nombre === "C") {
-    result.innerHTML = "";
-    operation.innerHTML = "";
-    preventDivisionByZero();
-    return;
-  }
-
-  if (equalBtnPressed) {
-    result.innerHTML = "";
-    equalBtnPressed = false;
-  }
-
-  if (nombre === "-") {
-    managingNegativeNumber();
-    return;
-  }
-
-  if (nombre === "+" || nombre === "/" || nombre === "*") {
-    if (result.innerHTML === "") {
-      changeOperator(nombre);
-      return;
-    }
-    if (mustPerformOperation()) {
-      performOperation();
-      result.innerHTML = "";
-      return;
-    }
-
-    operation.innerHTML = result.innerHTML + nombre;
-    result.innerHTML = "";
-  } else {
-    result.innerHTML += nombre;
-  }
-  preventDivisionByZero();
-}
-
-function preventDivisionByZero() {
-  if (operation.innerHTML === "") {
-    return;
-  }
-  if (
-    (/\//.test(operation.innerHTML) && result.innerHTML === "0") ||
-    (/\//.test(operation.innerHTML) && result.innerHTML === "0.") ||
-    (/\//.test(operation.innerHTML) && /^\..*/.test(result.innerHTML))
-  ) {
-    operatorBtns.forEach((operator) => {
-      operator.classList.add("disable-btn");
-    });
-  } else {
-    operatorBtns.forEach((operator) => {
-      operator.classList.remove("disable-btn");
-    });
-  }
-}
-
-function mustPerformOperation() {
-  return operators.test(operation.innerHTML);
-}
-
-function performOperation() {
-  isFloat = false;
-  const operator = operation.innerHTML.slice(operation.innerHTML.length - 1);
-  const leftSide = floatOrInt(operation.innerHTML);
-  const rightSide = floatOrInt(result.innerHTML);
-
-  switch (operator) {
-    case "+":
-      operation.innerHTML = leftSide + rightSide;
-      break;
-    case "-":
-      operation.innerHTML = leftSide - rightSide;
-      break;
-    case "*":
-      operation.innerHTML = leftSide * rightSide;
-      break;
-    case "/":
-      operation.innerHTML = leftSide / rightSide;
-      break;
-  }
-  if (isFloat || /\./.test(operation.innerHTML)) {
-    operation.innerHTML = parseFloat(operation.innerHTML).toFixed(4);
-  }
-  if (!equalBtnPressed) {
-    operation.innerHTML += operator;
-  }
-}
-
-function floatOrInt(params) {
-  if (/\./.test(operation.innerHTML) || /\./.test(result.innerHTML)) {
-    isFloat = true;
-    return parseFloat(params);
-  } else {
-    return parseInt(params);
-  }
-}
-
-function managingNegativeNumber() {
-  if (result.innerHTML === "") {
-    if (operation.innerHTML === "") {
-      result.innerHTML += "-";
+    if(operation.length == 0){
+      operation = result + text
+      show()
     } else {
-      changeOperator("-");
+      doOperation()
     }
-  } else {
-    if (operation.innerHTML != "") {
-      performOperation();
-      result.innerHTML = "";
-      return;
-    }
-    operation.innerHTML = result.innerHTML + "-";
-    result.innerHTML = "";
+    result = ""
+    show()
+    return
   }
+  if(text === "." && result.includes(".")) return
+  result += text
+  show()
 }
 
-function changeOperator(newOperator) {
-  let str = Array.from(operation.innerHTML);
-  str[operation.innerHTML.length - 1] = newOperator;
-  operation.innerHTML = str.join("");
+function show() {
+  resultCss.innerText = result
+  operationCss.innerText = operation
 }
 
 function createButton() {
 
-   for (let i = 0; i < 10; i++) {
-      const button = document.createElement("button");
-      button.classList.add("calc-btn", `calc-btn-${i}`);
-      button.innerText = i
-      calc.appendChild(button)
-   }
+  for (let i = 0; i < 10; i++) {
+     const button = document.createElement("button");
+     button.classList.add("calc-btn", `calc-btn-${i}`);
+     button.innerText = i
+     calc.appendChild(button)
+  }
+}
+
+function doOperation(){
+
+ const operator = operation.charAt(operation.length - 1)
+ switch (operator) {
+   case "+":
+     result = addition(parseFloat(operation),parseFloat(result))
+   break;
+   case "-":
+     result = soustraction(parseFloat(operation),parseFloat(result))
+   break;
+   case "*":
+     result = multiplication(parseFloat(operation),parseFloat(result))
+   break;
+   default:
+     result = division(parseFloat(operation),parseFloat(result))
+   break;
+ }
+ operation = ""
+ show()
+}
+
+function addition(leftHand, rightHand) {
+  return leftHand + rightHand
+}
+function soustraction(leftHand, rightHand) {
+  return leftHand - rightHand
+}
+function multiplication(leftHand, rightHand) {
+  return leftHand * rightHand
+}
+function division(leftHand, rightHand) {
+  return leftHand / rightHand
 }
